d910fe01f1e0812dce0a3b162e7f6381bc5a6e13
Dabid <davidezekielgarin@gmail.com>
2022-04-27 14:42:35 +0800

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="icon" href="https://i.ibb.co/G3LH2x9/cart-icon.png">
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.css" integrity="sha512-1hsteeq9xTM5CX6NsXiJu3Y/g+tj+IIwtZMtTisemEv3hx+S9ngaW4nryrNcPM4xGzINcKbwUJtojslX2KG+DQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <table class="table">
+            <center><br> <br><h1> Admin </h1></center>
+            <hr><br>
+            <tr>
+                <th>Product Name</th>
+                <th>Product Price (₱)</th>
+                <th>Image URL</th>
+                <th>Product Description</th>
+            </tr>
+            <tbody>
+                <?php include 'php/read.php'; ?>
+            </tbody>
+        </table>
+        <br> <br>
+        <h5> Add Entry </h5>
+        <hr>
+        <form class="form-inline m-2" action="php/add.php" method="POST">
+            <label for="prod_name">Name:</label>
+            <input type="text" class="form-control m-2" id="prod_name" name="prod_name">
+            <label for="prod_price">Price:</label>
+            <input type="number" class="form-control m-2" id="prod_price" name="prod_price">
+            <label for="prod_image">Image URL:</label>
+            <input type="text" class="form-control m-2" id="prod_image" name="prod_image">
+            <label for="prod_desc">Description:</label>
+            <input type="text" class="form-control m-2" id="prod_desc" name="prod_desc">
+            <center> <button type="submit" class="btn btn-primary" style="margin-top: 25px;">Submit</button> </center>
+        </form>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
