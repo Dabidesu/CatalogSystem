49e6126a8cb02aa0100d73d2b05e25b2d6b82d20
Dabidesu <davidezekielgarin@gmail.com>
2022-05-11 03:24:14 +0800

    initial commit

--- php/db.php
+++ php/db.php
@@ -1,8 +1,8 @@
 <?php
-    $db_host = "localhost";
-    $db_username = "root";
-    $db_pass = "";
-    $db_table = "catalogdb";
+    $db_host = "sql113.epizy.com";
+    $db_username = "epiz_31696241";
+    $db_pass = "epiz_31696241";
+    $db_table = "epiz_31696241_catalogdb";
 
     $con = new mysqli($db_host, $db_username, $db_pass, $db_table) or die(mysqli_error());
 
