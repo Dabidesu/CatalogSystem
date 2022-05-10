3ff2a0c3cf0914cf2212aaf0ad06f9580020e82e
Torquila-stack <60341216+Torquila-stack@users.noreply.github.com>
2022-05-10 22:36:21 +0800

    Change links for catalog

--- LandingPage.php
+++ LandingPage.php
@@ -45,7 +45,7 @@
                     <ul class="navbar-nav nav-right">
                         <li class="nav-item"><a class="nav-link active" href="LandingPage.html" style="color: rgba(224,217,217,0.9);">Home </a></li>
                         <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#" style="color: rgba(224,217,217,0.9);">Catalog</a>
-                            <div class="dropdown-menu"><a class="dropdown-item" href="#" style="color: rgb(255,255,255);">Mini PC</a><a class="dropdown-item" href="#" style="color: rgb(255,255,255);">Single-Board Computers</a></div>
+                            <div class="dropdown-menu"><a class="dropdown-item" href="index.php" style="color: rgb(255,255,255);">Mini PC</a><a class="dropdown-item" href="index.php" style="color: rgb(255,255,255);">Single-Board Computers</a></div>
                         </li>
                         <li class="nav-item"><a class="nav-link" href="#" style="color: var(--bs-gray-500);">PC Builder</a></li>
                         <li class="nav-item"><a class="nav-link" href="About.php" style="color: rgba(224,217,217,0.9);">About </a></li>
